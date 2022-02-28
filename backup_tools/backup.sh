ab14566a7ce9816c3f6c180e4c6c58db00dd9c58
Valentine Gutkin <valentineg@radware.com>
2022-02-28 14:26:51 +0000

    Add backup and restore script

---
+++ backup_tools/backup.sh
@@ -0,0 +1,164 @@
+#! /bin/bash
+
+
+# Help\Usage message
+usage() {
+  echo '''
+  This script will backup or restore waas configuration objects
+  '''
+  echo "Usage:"
+  echo "    $(basename "$0") [options]"
+  echo "Backup:"
+  echo "    $(basename "$0") --backup"
+  echo "Restore:"
+  echo "    $(basename "$0") -r -i waas_backup.tgz"
+  echo '''
+Options:
+    -h|--help               Print usage
+    -b|--backup             Generate backup archive
+    -r|--restore            Perform restore operation, use [-i or --input] to provide input archive 
+    -i|--input              Change default input filename
+  '''
+}
+
+invalid() {
+  echo "[ERROR] Unrecognized argument: $1" >&2
+  usage
+  exit 1
+}
+# Parse options
+END_OF_OPT=
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  case "${END_OF_OPT}${1}" in
+    -h|--help)      usage; exit 0 ;;
+    -i|--input)     shift;BACKUP_TAR="$1";;
+    -b|--backup)    BACKUP=1;;
+    -r|--restore)   RESTORE=1;;
+    --)             END_OF_OPT=1 ;;
+    *)              POSITIONAL+=("$1") ;;
+  esac
+  shift
+done
+
+EXTRACT_DIR="tmp_restore_dir"
+ERRORS=()
+TMP_ERROR=""
+
+#List of CRDs to backup
+CRD_TYPES=('apispecs' 'decodingbehaviors' 'mappings' 'openapis' 'profiles' 'segments' 'sourcegroups' 'unblockrequests')
+
+#List of ConfigMaps to backup
+CONFIG_MAPS_NAMES=('waas-activity-tracker-config' 'waas-ca-config' 'waas-custom-rules-configmap' 'waas-elasticsearch-ilm-options-config' 'waas-elasticsearch-jvm-options-config' 'waas-identity-auth-config' 'waas-licenses-configmap' 'waas-logstash-jvm-options-config' 'waas-logstash-pipeline-config' 'waas-logstash-templates-config' 'waas-prometheus-config' 'waas-redis-init-config' 'waas-request-data-configmap')
+
+#Kubectl patch parameters for removing fields
+PATCH_STRING="{\"op\": \"remove\", \"path\": \"/metadata/uid\"}, \
+{\"op\": \"remove\", \"path\": \"/metadata/resourceVersion\"}, \
+{\"op\": \"remove\", \"path\": \"/metadata/selfLink\"}, \
+{\"op\": \"remove\", \"path\": \"/metadata/creationTimestamp\"}"
+
+#Make sure ConfigMap output file is empty
+function cm_backup {
+    echo "" > "cm_backup.yml"
+    for CM in "${CONFIG_MAPS_NAMES[@]}"; do
+        #Get full configmap definition 
+        OBJ=$(kubectl get cm --ignore-not-found --all-namespaces --field-selector metadata.name=$CM -o jsonpath='{.items[]}') 
+        
+        #Check if CRD has "metadata/annotations/kubectl.kubernetes.io~1last-applied-configuration" field
+        LAST_APPLIED_ANNO=$(kubectl get cm --ignore-not-found --all-namespaces --field-selector metadata.name=$CM -o jsonpath='{.metadata.annotations.kubectl\.kubernetes\.io~1last-applied-configuration}')
+        TMP_PATCH_STRING=$PATCH_STRING
+        if [[ ! -z $LAST_APPLIED_ANNO ]]; then
+            #In case last-applied-configuration annotation is found - add it to removal
+            TMP_PATCH_STRING="$TMP_PATCH_STRING, {\"op\": \"remove\", \"path\": \"/metadata/annotations/kubectl.kubernetes.io~1last-applied-configuration\"}"
+        fi
+        
+        #Remove fields based on the parameter value above
+        echo $OBJ | kubectl patch -f - --dry-run=client --type=json --patch="[$TMP_PATCH_STRING]" -o yaml >> "cm_backup.yml"
+
+        #Add delimiter to output file
+        echo "---" >> "cm_backup.yml"
+    done
+}
+
+function crd_backup {
+    for CRD_TYPE in "${CRD_TYPES[@]}"; do
+        #Make sure CRD output file is empty
+        echo "" > "${CRD_TYPE}_backup.yml"
+        #get all CRD names and namespaces, delimited by ";;"
+        for CRD in $(kubectl get $CRD_TYPE --all-namespaces --ignore-not-found -o jsonpath='{range .items[*]}{@.metadata.name}{";;"}{@.metadata.namespace}{"\n"}{end}'); do
+            #Extract CRD name - the string up to ";;"
+            NAME=${CRD%;;*}
+            
+            #Extract CRD namespace - the string after ";;"
+            NS=${CRD#*;;}
+            
+            #Get full CRD definition 
+            OBJ=$(kubectl get $CRD_TYPE --ignore-not-found --namespace $NS $NAME -o json)
+            OBJ=$(kubectl get $CRD_TYPE --ignore-not-found --namespace $NS $NAME -o json)
+            
+            #Check if CRD has "status" or "metadata/annotations/kubectl.kubernetes.io~1last-applied-configuration" fields
+            STATUS=$(kubectl get $CRD_TYPE --ignore-not-found --namespace $NS $NAME -o jsonpath='{.status}')
+            LAST_APPLIED_ANNO=$(kubectl get $CRD_TYPE --ignore-not-found --namespace $NS $NAME -o jsonpath='{.metadata.annotations.kubectl\.kubernetes\.io~1last-applied-configuration}')
+            TMP_PATCH_STRING=$PATCH_STRING
+            
+            if [[ ! -z $STATUS ]]; then
+                #In case status field found - add removal of "status" field
+                TMP_PATCH_STRING="$TMP_PATCH_STRING, {\"op\": \"remove\", \"path\": \"/status\"}"
+            fi
+            if [[ ! -z $LAST_APPLIED_ANNO ]]; then
+                #In case last-applied-configuration annotation is found - add it to removal
+                TMP_PATCH_STRING="$TMP_PATCH_STRING, {\"op\": \"remove\", \"path\": \"/metadata/annotations/kubectl.kubernetes.io~1last-applied-configuration\"}"
+            fi
+            echo $OBJ | kubectl patch -f - --dry-run=client --type=json --patch="[$TMP_PATCH_STRING]" -o yaml >> "${CRD_TYPE}_backup.yml"
+            
+            #Add delimiter to output file
+            echo "---" >> "${CRD_TYPE}_backup.yml"
+        done
+    done
+}
+
+function recover_backup {
+	mkdir ./$EXTRACT_DIR
+	tar -xzf $BACKUP_TAR -C $EXTRACT_DIR
+
+	FILES=($(ls $EXTRACT_DIR))
+	for backup_file in "${FILES[@]}"
+	do
+		TMP_ERROR=$(kubectl apply -f $EXTRACT_DIR/$backup_file 2>&1 > /dev/null)
+		if [[ $TMP_ERROR != "" ]];
+		then
+			ERRORS+=("$backup_file failed to apply: $TMP_ERROR")
+			TMP_ERROR=""
+		else
+			echo "$backup_file applied successfully"
+		fi
+	done
+
+	rm -rf $EXTRACT_DIR
+
+	if [[ ${#ERRORS[@]} != 0 ]];
+	then
+		for err in "${ERRORS[@]}"
+		do
+			echo -e "\n$err\n"
+		done
+		exit 1
+	else
+		echo -e "\nRecovery finished successfully\n"
+		exit 0
+	fi
+}
+
+if [[ ! -z $RESTORE ]]; then
+    if [[ -z $BACKUP_TAR ]]; then
+        echo "[ERROR] no backup archive provided!"
+        echo "use [-i or --input] to provide input archive"
+        exit 2
+    fi
+    recover_backup
+fi
+if [[ ! -z $BACKUP ]]; then
+    cm_backup
+    crd_backup
+    tar -czvf "waas_backup_$(date "+%d-%m-%y").tgz" *.yml
+fi
