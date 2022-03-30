ed84d6ccaa5806d2aac8a96ecf010fc0decb5c0d
Valentine Gutkin <valentineg@radware.com>
2022-03-30 12:00:35 +0000

    remove nested loop. fix bug in object containing "/n"

--- backup_tools/restore.sh
+++ backup_tools/restore.sh
@@ -2,6 +2,8 @@
 
 ERRORS=()
 TMP_ERROR=""
+OBJECT=""
+OBJECT_DELIMITER="---"
 
 #Backup original IFS (word seperator)
 OLD_IFS="$IFS"
@@ -22,59 +24,63 @@ else
     INPUT="$(cat)"
 fi
 
-#Split the input to an array, each k8s object is an element
-i=0
-INPUT_ARRAY=()
-for item in $INPUT; do
-    if [[ $item == "---" ]]; then 
-        i+=1
-        continue
-    fi
-    INPUT_ARRAY[$i]+="$item\\n"
-done
-
 function recover_backup {
-	#Iterate over the backup objects
-	for object in "${INPUT_ARRAY[@]}"; do
-        #Get object name, ns and kind
-        BACKUP_NAME=$(echo -e "$object" | kubectl apply -f - --dry-run=client -o jsonpath='{.metadata.name}')
-        BACKUP_NS=$(echo -e "$object" | kubectl apply -f - --dry-run=client -o jsonpath='{.metadata.namespace}') 
-        BACKUP_KIND=$(echo -e "$object" | kubectl apply -f - --dry-run=client -o jsonpath='{.kind}') 
-        
-        #Verify object NS exists, if not - create it
-        if [[ -z $(kubectl get namespace $BACKUP_NS --ignore-not-found ) ]]; then 
-            kubectl create namespace $BACKUP_NS
-        fi
+    #Get object name, ns and kind
+    BACKUP_NAME=$(echo "$OBJECT" | kubectl apply -f - --dry-run=client -o jsonpath='{.metadata.name}')
+    BACKUP_NS=$(echo "$OBJECT" | kubectl apply -f - --dry-run=client -o jsonpath='{.metadata.namespace}') 
+    BACKUP_KIND=$(echo "$OBJECT" | kubectl apply -f - --dry-run=client -o jsonpath='{.kind}') 
 
-        #Check if resource already exists
-        RESOURCE=$(kubectl get $BACKUP_KIND --ignore-not-found --namespace $BACKUP_NS $BACKUP_NAME --no-headers)
+    #Verify object NS exists, if not - create it
+    if [[ -z $(kubectl get namespace $BACKUP_NS --ignore-not-found ) ]]; then 
+        kubectl create namespace $BACKUP_NS
+    fi
 
-        if [[ -n $RESOURCE ]]; then
-            #Resource exists, use 'replace' to restore the object
-            TMP_ERROR=$(echo -e "$object" | kubectl replace -f - 2>&1 > /dev/null | sed "/patched automatically/d")
-        else
-            #Resource doesn't exists, use 'apply' to restore the object
-            TMP_ERROR=$(echo -e "$object" | kubectl apply -f - 2>&1 > /dev/null | sed "/patched automatically/d")
-        fi
+    #Check if resource already exists
+    RESOURCE=$(kubectl get $BACKUP_KIND --ignore-not-found --namespace $BACKUP_NS $BACKUP_NAME --no-headers)
 
-        if [[ $TMP_ERROR != "" ]]; then
-            ERRORS+=("$BACKUP_NAME in namespace $BACKUP_NS type $BACKUP_KIND failed to apply: $TMP_ERROR")
-            TMP_ERROR=""
-        else
-            echo "$BACKUP_NAME in namespace $BACKUP_NS type $BACKUP_KIND applied successfully"
-        fi
-	done
 
-	if [[ ${#ERRORS[@]} != 0 ]]; then
-		for err in "${ERRORS[@]}"; do
-			echo -e "\n$err\n"
-		done
-		exit 1
-	else
-		echo -e "\nRecovery finished successfully\n"
-		exit 0
-	fi
+    if [[ -n $RESOURCE ]]; then
+        #Resource exists, use 'replace' to restore the object
+        TMP_ERROR=$(echo "$OBJECT" | kubectl replace -f - 2>&1 > /dev/null | sed "/patched automatically/d")
+    else
+        #Resource doesn't exists, use 'apply' to restore the object
+        TMP_ERROR=$(echo "$OBJECT" | kubectl apply -f - 2>&1 > /dev/null | sed "/patched automatically/d")
+    fi
+
+    if [[ $TMP_ERROR != "" ]]; then
+        ERRORS+=("$BACKUP_NAME in namespace $BACKUP_NS type $BACKUP_KIND failed to apply: $TMP_ERROR")
+        TMP_ERROR=""
+    else
+        echo "$BACKUP_NAME in namespace $BACKUP_NS type $BACKUP_KIND applied successfully"
+    fi
 }
 
+#In case input containes more than one objcet (Delimiter-separated)
+#Iterate over each object performing the "recover_backup" function
+#Otherwise perform the operation on the entire input
+if [[ "$INPUT" == *"$OBJECT_DELIMITER"* ]]; then
+     for item in $INPUT; do
+        if [[ $item == "---" ]]; then 
+            if [[ $OBJECT != $OBJECT_DELIMITER$IFS ]]; then
+                recover_backup
+            fi 
+            OBJECT=""
+        fi
+        OBJECT+="$item$IFS"
+    done
+else
+    OBJECT=$INPUT
+    recover_backup
+fi
+
+if [[ ${#ERRORS[@]} != 0 ]]; then
+    for err in "${ERRORS[@]}"; do
+        echo -e "\n$err\n"
+    done
+    exit 1
+else
+    echo -e "\nRecovery finished successfully\n"
+    exit 0
+fi
+
 IFS=$OLD_IFS
-recover_backup
